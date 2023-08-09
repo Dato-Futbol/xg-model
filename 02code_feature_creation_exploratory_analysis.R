5b1f0b72306c1172502c3f38fcdd9e2332382ed9
Ismael Gómez Schmidt <ismaelgomezs@gmail.com>
2023-08-09 17:32:07 -0500

    update variables names due package changes

--- 02code_feature_creation_exploratory_analysis.R
+++ 02code_feature_creation_exploratory_analysis.R
@@ -13,14 +13,14 @@ shotsB <- shots %>%
          time_prev = ifelse(is.na(time_prev), -1, time_prev),
          skilled_foot = ifelse(body_part == "head/body", body_part,
                                ifelse(body_part == foot, "Yes", "No")),
-         x_meter = x_1 * 105/100,
-         y_meter = y_1 * 68/100,
+         x_meter = x1 * 105/100,
+         y_meter = y1 * 68/100,
          distance_to_goal_line = sqrt((105 - x_meter)^2 + (32.5 - y_meter)^2),
          angle_to_goal = atan( (7.32 * (105 - x_meter) ) / ( (105 - x_meter)^2 + (32.5 - y_meter)^2 - (7.32/2)^2 )) * 180/pi) %>%
   filter(!is.na(skilled_foot))
 
 data_to_mod <- shotsB %>%
-        dplyr::select(is_goal, eventSec, matchPeriod, x_1, y_1, is_CA, 
+        dplyr::select(is_goal, eventSec, matchPeriod, x1, y1, is_CA, 
                       time_prev, skilled_foot, distance_to_goal_line, angle_to_goal) %>%
         mutate(is_goal = factor(is_goal),
                matchPeriod = factor(matchPeriod),
